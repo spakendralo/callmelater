de1727dddaf501f66cc9a1ac13c34c12f174a766
spakendralo <spakendralo@gmail.com>
2020-01-18 15:27:48 +0100

    Git Heroku pipeline test

--- src/main/java/lu/pistache/callmelater/GreetingController.java
+++ src/main/java/lu/pistache/callmelater/GreetingController.java
@@ -1,7 +1,9 @@
 package lu.pistache.callmelater;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.squareup.okhttp.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +14,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     Logger logger = LoggerFactory.getLogger(GreetingController.class);
+    final String DEX_PROD = "api.dexcom.com";
+    final String DEX_SAND = "sandbox-api.dexcom.com";
+    String DEX_HOST = DEX_SAND;
 
 
     @GetMapping("/greeting")
-    public void greeting(@RequestParam(value = "code") String code, @RequestParam(value = "state", defaultValue = "") String state) {
-        logger.info("Got code" + code);
-        //return new GreetingReply(0, "Returned " + code);
+    public void greeting(@RequestParam(value = "code") String code, @RequestParam(value = "state", defaultValue = "") String state) throws IOException {
+        logger.info("Got code " + code);
+        OkHttpClient client = new OkHttpClient();
+
+        String clientId = "weP9DT4racO6xmu5iDG94M7j73bpqbW5";
+        String clientSecret = "mpoSSYGpd5owOmvX";
+
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "client_secret=" +
+                clientSecret +
+                "&client_id=" +
+                clientId +
+                "&code=" +
+                code +
+                "&grant_type=authorization_code&redirect_uri=" +
+                "{your_redirect_uri}" +
+                "");
+        Request request = new Request.Builder()
+                .url("https://" + DEX_HOST + "/v2/oauth2/token")
+                .post(body)
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .addHeader("cache-control", "no-cache")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        logger.info("Got access token response " + response.toString());
     }
 }
